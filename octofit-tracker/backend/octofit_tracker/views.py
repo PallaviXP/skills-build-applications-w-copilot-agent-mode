78260ab832e927e937fb53eefb5c4a6c08a10223
PallaviXP <24568150+PallaviXP@users.noreply.github.com>
2025-04-08 10:57:12 +0000

    re-iterate steps

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,12 +1,19 @@
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
+from .serializers import (
+    UserSerializer,
+    TeamSerializer,
+    ActivitySerializer,
+    LeaderboardSerializer,
+    WorkoutSerializer,
+)
 from .models import User, Team, Activity, Leaderboard, Workout
+
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'https://congenial-rotary-phone-gx746gx5xjj29p7x-8000.app.github.dev/'
+    base_url = 'https://improved-spork-wvgrxwvvwp7f9g96.github.dev-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/?format=api',
         'teams': base_url + 'api/teams/?format=api',
@@ -15,21 +22,26 @@ def api_root(request, format=None):
         'workouts': base_url + 'api/workouts/?format=api'
     })
 
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+
 class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
 
+
 class LeaderboardViewSet(viewsets.ModelViewSet):
     queryset = Leaderboard.objects.all()
     serializer_class = LeaderboardSerializer
+
 
 class WorkoutViewSet(viewsets.ModelViewSet):
     queryset = Workout.objects.all()
