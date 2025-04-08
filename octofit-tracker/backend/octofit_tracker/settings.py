315a34aceb01cd378e1b8bf5dbaea61507168c36
PallaviXP <24568150+PallaviXP@users.noreply.github.com>
2025-04-08 06:06:27 +0000

    Your commit message here

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -73,10 +73,13 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Updated database settings for MongoDB
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "djongo",
+        "NAME": "monafit_db",
+        "HOST": "localhost",
+        "PORT": 27017,
     }
 }
 
