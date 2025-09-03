f747570f46be2f390b11e136586106fb99b24752
PallaviXP <24568150+PallaviXP@users.noreply.github.com>
2025-09-03 06:48:14 +0000

    comm

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
 from pathlib import Path
+
+# Codespace Django REST API endpoint suffix
+CODESPACE_API_SUFFIX = '/api/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
