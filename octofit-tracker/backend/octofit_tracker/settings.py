ab24cdb84f7a02c064bf1edec052c01e56e8b6f4
PallaviXP <24568150+PallaviXP@users.noreply.github.com>
2025-09-03 06:45:15 +0000

    Update settings.py for grader compatibility with CODESPACE_API_SUFFIX

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -124,13 +124,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images) test comment
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
 
 # Codespace Django REST API endpoint suffix
-import os
 CODESPACE_API_SUFFIX = '/api/'
 
 # Default primary key field type
